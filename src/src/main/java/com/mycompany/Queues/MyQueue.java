bde8665a494e15dd90533e355d2a45c85646d5f3
Abhik Banerjee <abhik.banerjee@kohls.com>
2017-03-29 14:30:52 -0700

    refactored some of the code to move it to proper locations

--- src/src/main/java/com/mycompany/Queues/MyQueue.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.Queues;
-public interface MyQueue {
-	public void enqueue(Object obj);
-	public Object dequeue();
-	public Object front();
-	public int size(MyQueue s);
-	public boolean isEmpty(MyQueue s);
-	public void printQueue();
-}
