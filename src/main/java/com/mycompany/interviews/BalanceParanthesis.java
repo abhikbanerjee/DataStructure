f34f24ba017e6cf9e068a1b6ff7e2245d8b1edd6
Abhik Banerjee <abhik.banerjee@kohls.com>
2017-04-01 11:02:55 -0700

    Balance Paranthesis problem and moving zeros to the right problem

---
+++ src/main/java/com/mycompany/interviews/BalanceParanthesis.java
@@ -0,0 +1,55 @@
+package com.mycompany.interviews;
+
+import java.util.Stack;
+
+/**
+ * Created by Abhik Banerjee
+ * on 4/1/17.
+ */
+public class BalanceParanthesis
+{
+    public static void main(String args[])
+    {
+        //String expn = "(abc(def[478]hj)jnjn)";
+        String expn = "(abc(def[478]hj]jnjn)";
+        //String expn = "(abc(def[478]hj))jnjn)";
+        //String expn = "";
+        BalanceParanthesis bp = new BalanceParanthesis();
+        System.out.println("Is the Expression Balanced - "+ bp.isParanBalanced(expn));
+    }
+
+    public boolean isParanBalanced(String expn)
+    {
+        boolean isBalanced = true;
+        Stack<String> operators = new Stack<String>();
+        for(int i=0;i<expn.length();i++)
+        {
+            String ch = expn.charAt(i)+"";
+            if(ch.equals("{") || ch.equals("(") || ch.equals("["))
+            {
+                operators.push(ch);
+            }
+            if(ch.equals("}") || ch.equals(")") || ch.equals("]"))
+            {
+                if(operators.empty())
+                {
+                    isBalanced=false;
+                    break;
+                }
+                String poppedChar = operators.pop();
+                if( (ch.equals("}") && !poppedChar.equals("{")) ||
+                        (ch.equals("]") && !poppedChar.equals("[")) ||
+                (ch.equals(")") && !poppedChar.equals("(")) )
+                {
+                    isBalanced=false;
+                    break;
+                }
+            }
+        }
+        if(isBalanced==true && operators.size()==0)
+            return true;
+        else
+            return false;
+    }
+
+}
---
+++ src/main/java/com/mycompany/interviews/MoveZerosRight.java
@@ -0,0 +1,52 @@
+package com.mycompany.interviews;
+
+/**
+ * Created by Abhik Banerjee
+ * on 4/1/17.
+ */
+public class MoveZerosRight
+{
+    public static void main(String args[])
+    {
+        Integer arr[] = new Integer[]{1,4,0,3,0,7,0,6,2,9};
+        MoveZerosRight mv = new MoveZerosRight();
+        Integer arr1[] = mv.moveZerosRight(arr);
+        System.out.println("Printing the array after moving zeros");
+        for(int i=0;i<arr1.length;i++)
+        {
+            System.out.print(arr1[i]+" ");
+        }
+        System.out.println();
+
+    }
+
+    public Integer[] moveZerosRight(Integer arr[])
+    {
+        //Base Condition
+        if(arr==null || arr.length==0)
+        {
+            System.out.println("Array is Blank");
+            return arr;
+        }
+
+        int lowerIndex = 0;
+        int upperInDex = arr.length-1;
+        //Breaking condition continue the loop untill there are no more places to fill zeros
+        while(lowerIndex <= upperInDex)
+        {
+            //If lower index is zero swap the values of lower and upper indexes and update the counters accordingly
+            if(arr[lowerIndex]==0)
+            {
+                arr[lowerIndex] = arr[upperInDex];
+                arr[upperInDex] = 0;
+                lowerIndex++;
+                upperInDex--;
+            }
+            else
+            {
+                lowerIndex++;
+            }
+        }
+        return arr;
+    }
+}
