b35c31cb8be49ef4b0438f7a0f8aaaa64379dde0
Abhik Banerjee <abhik.banerjee@kohls.com>
2017-04-10 16:01:37 -0700

    count take steps 1, 2 or 3

---
+++ src/main/java/com/mycompany/interviews/CountTakeSteps.java
@@ -0,0 +1,33 @@
+package com.mycompany.interviews;
+
+/**
+ * Created by Abhik Banerjee
+ * on 4/7/17.
+ */
+
+// Lets say we can take 1, 2 or 3 steps and given N steps how many different ways we can take those N steps
+public class CountTakeSteps
+{
+    public static void main(String[] args)
+    {
+        CountTakeSteps ct = new CountTakeSteps();
+        Integer steps = 4;
+        //Integer steps = 9;
+        System.out.println("Number of Ways one can take N steps - " + ct.countSteps(steps));
+    }
+
+    public int countSteps(Integer steps)
+    {
+        if(steps < 0 )
+        {
+            return 0;
+        }
+        else if(steps==0)
+        {
+            return 1;
+        }
+        else {
+            return countSteps(steps-1) + countSteps(steps-2) + countSteps(steps-3);
+        }
+    }
+}
